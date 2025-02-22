b70382acdb1953bfb0cdfecb37af7c3f53cae944
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-02-22 17:39:02 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".hero-swiper", {
+    loop: false,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: false, // Remove next & prev buttons
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var circularSwiper = new Swiper(".circular-swiper", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
