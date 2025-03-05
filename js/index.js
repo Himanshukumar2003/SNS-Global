0ed0f83868366de90cfa76682a45c4ebe7561bb7
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-03-05 12:56:35 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -74,11 +74,12 @@ var swiper = new Swiper('.banner', {
 var swiper = new Swiper(".clints", {
     slidesPerView: 4,
     spaceBetween: 20,
-    loop: true,
-    autoplay: {
-        delay:800,
-        disableOnInteraction: false,
-    },
+    loop: false,
+      autoplay:false,
+      // {
+    //     delay:800,
+    //     disableOnInteraction: false,
+    // },
     breakpoints: {
         768: {
             slidesPerView: 4,
