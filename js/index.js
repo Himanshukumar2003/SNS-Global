6e307dd8997f7013f18324fb487ad57359b9356f
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-03-08 17:03:31 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -14,6 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+var swiper = new Swiper(".our-service", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination:false,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
+    }
+});
 
 
 
